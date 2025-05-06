4a917595cc2b9c9e8f535d3eaf87cec9bbbae21f
nailartnty <nnailartnty.06@gmail.com>
2025-05-06 21:10:30 +0700

    masukin api keynya di file .env, daftarin asset di pubspec.yaml, mulai koding di file gemini_service.dart

--- lib/services/gemini_service.dart
+++ lib/services/gemini_service.dart
@@ -1,3 +1,31 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class GeminiService {
-  static const String _baseUrl = 'input your base url here';
+  // untuk gerbang awal antara client(kode project) sama server(gemini api)
+  static const String _baseUrl = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent";
+
+  final String apiKey;
+
+  /*
+    ini adalah sebuah ternary operator untuk mematikan 
+    apakah nilai dari API KEY tersedia atau kosong
+  */
+  GeminiService() : apiKey = dotenv.env["GEMINI_API_KEY"] ?? "" { // ini syntax yg pendek dari ternary operator biar clean code 
+    if (apiKey.isEmpty) {
+      throw ArgumentError("Please input Your API KEY"); // error message
+    }
+  }
+
+  // logika untuk generating result dari input yang diberikan
+  // yang akan diotomasi oleh AI API
+  Future<String> generateSchedule(List<Task> tasks) async {
+    _validateTasks(tasks); // kalau misal dia static ditandai _ didepannya
+    final prompt = _buildPrompt(tasks);
+
+    try {
+      
+    } catch (e) {
+      
+    }
+  }
 }
